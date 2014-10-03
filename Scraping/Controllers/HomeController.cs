1a173c60265d40ec817282191a7825f5c8cba3f2
Smygi <m.melbing@gmail.com>
2014-10-03 11:18:58 +0200

    Scraping

--- Scraping/Controllers/HomeController.cs
+++ Scraping/Controllers/HomeController.cs
@@ -2,8 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.InteropServices;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
+using Scraping.Infrastructure;
+using Scraping.Models;
 
 namespace Scraping.Controllers
 {
@@ -14,16 +17,19 @@ namespace Scraping.Controllers
 
         public ActionResult Index()
         {
-            return View();
+            return View(new IndexViewModel());
         }
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Index(string orgNr)
+        public async Task<ActionResult> Index(string orgNr)
         {
             if (string.IsNullOrEmpty(orgNr))
                 return View();
-            return View();
+            var client = new Client(orgNr);
+            await client.Start();
+            var company = DissectHtmlPage.Dissect(client.Content);
+            return View(new IndexViewModel { CompanyName = company });
         }
     }
 }
---
+++ Scraping/Models/IndexViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Scraping.Models
+{
+    public class IndexViewModel
+    {
+        public string CompanyName { get; set; }
+    }
+}
