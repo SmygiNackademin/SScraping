434a403b9c6e83c9ea9d924dd4b9d9328fdc5450
Smygi <m.melbing@gmail.com>
2014-10-03 10:10:54 +0200

    Init bitches

---
+++ Scraping/Infrastructure/Client.cs
@@ -0,0 +1,23 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Scraping.Infrastructure
+{
+    public class Client
+    {
+        public string Content { get; set; }
+        private string address = "http://allabolag.se/";
+        HttpClient _client = new HttpClient();
+
+        public Client(string orgNr)
+        {
+            address += orgNr;
+        }
+
+        public async Task Start()
+        {
+            var response = await _client.GetAsync(address);
+            Content = await response.Content.ReadAsStringAsync();
+        }
+    }
+}
